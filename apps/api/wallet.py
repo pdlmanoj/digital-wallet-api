1e097902b3943244409089bc9428f3143132f7c3
Manoj Paudel <mpsandip12@gmail.com>
2026-08-21 14:54:42 +0545

    feat(wallet-api): refactor wallet creation logic to use get_user_wallet function for existence check

--- apps/api/wallet.py
+++ apps/api/wallet.py
@@ -36,10 +36,9 @@ def create(
             detail="Balance cannot be negative or 0",
         )
 
-    smth = select(Wallet).where(Wallet.user_id == current_user.id)
-    is_exist = db.scalars(smth).one_or_none()
+    query = get_user_wallet(current_user.id, db)
 
-    if is_exist:
+    if query:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="It seems you already have a wallet account with us. You can't recreate new wallet again.",
